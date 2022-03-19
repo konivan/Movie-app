debc3ef6841ed5ff376bd19300389233ead754d0
konivan <konivan08@gmail.com>
2022-03-19 18:40:30 +0300

    work on header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
-import './App.css';
+import './App.scss';
+import React from "react";
+import {Routes, Route} from "react-router-dom";
+import Home from './components/Home/Home';
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import PageNotFound from './components/PageNotFound/PageNotFound';
+import MovieDetail from './components/MovieDetail/MovieDetail';
 
 function App() {
   return (
-    <div className="App">
-      App
+    <div className="app">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:imdbID" element={<MovieDetail />} />
+        <Route element={<PageNotFound />} />
+      </Routes>
+      <Footer />
     </div>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
+import './Header.scss';
+import user from '../../images/user.png';
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className="header">
+      <NavLink to="/">
+        <div className="logo">Movie App</div>
+      </NavLink>
+      <div className="user-image">
+        <img src={user} alt="user" />
+      </div>
     </div>
   );
 };
