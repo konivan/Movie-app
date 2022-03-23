39920d51d0fc7df74f2bfffd40bc51d5de0d1354
konivan <konivan08@gmail.com>
2022-03-23 20:41:13 +0300

    state movieDetail

--- src/components/MovieDetail/MovieDetail.jsx
+++ src/components/MovieDetail/MovieDetail.jsx
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useParams } from 'react-router';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchAsyncMovieOrShowDetail, getSelectedMovieOrShow } from '../../features/movies/movieSlice';
 
 const MovieDetail = () => {
+  const {imdbID} = useParams();
+  const dispatch = useDispatch();
+  const data = useSelector(getSelectedMovieOrShow);
+  useEffect(() => {
+    dispatch(fetchAsyncMovieOrShowDetail(imdbID))
+  },[dispatch, imdbID]);
   return (
     <div>
       MovieDetail
--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -24,9 +24,20 @@ export const fetchAsyncShows = createAsyncThunk(
   }
 );
 
+export const fetchAsyncMovieOrShowDetail = createAsyncThunk(
+  "movies/fetchAsyncMovieOrShowDetail",
+  async (id) => {
+    const response = await movieApi.get(
+      `?apiKey=${APIKey}&i=${id}&Plot=full`
+    );
+    return response.data;
+  }
+);
+
 const initialState = {
   movies: {},
   shows: {},
+  selectMovieOrShow: {},
 }
 
 const movieSlice = createSlice({
@@ -52,10 +63,15 @@ const movieSlice = createSlice({
       console.log("Fetched Ok");
       return {...state, shows: payload};
     },
+    [fetchAsyncMovieOrShowDetail.fulfilled]: (state, { payload }) => {
+      console.log("Fetched Ok");
+      return {...state, selectMovieOrShow: payload};
+    },
   },
 });
 
 export const { addMovies } = movieSlice.actions;
 export const getAllMovies = (state) => state.movies.movies;
 export const getAllShows = (state) => state.movies.shows;
+export const getSelectedMovieOrShow = (state) => state.movies.selectMovieOrShow;
 export default movieSlice.reducer;
